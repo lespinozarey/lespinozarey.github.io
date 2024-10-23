449a1749ef7024b82e8f87d201063ce3e2a644ac
lespinozarey <leonardoespinozarey@gmail.com>
2024-10-23 19:44:22 -0300

    efecto css main

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-const dynamicText = document.querySelector("h1 span");
-const words = ["Señalética", "Protección Personal", "#", "#"];
+const dynamicText = document.querySelector("h1 span ");
+const words = ["Señalética          ", "Protección Personal          "];
 
 // Variables to track the position and deletion status of the word
 let wordIndex = 0;
@@ -15,17 +15,17 @@ const typeEffect = () => {
     if (!isDeleting && charIndex < currentWord.length) {
         // If condition is true, type the next character
         charIndex++;
-        setTimeout(typeEffect, 200);
+        setTimeout(typeEffect, 80);
     } else if (isDeleting && charIndex > 0) {
         // If condition is true, remove the previous character
         charIndex--;
-        setTimeout(typeEffect, 100);
+        setTimeout(typeEffect, 80);
     } else {
         // If word is deleted then switch to the next word
         isDeleting = !isDeleting;
         dynamicText.classList.remove("stop-blinking");
         wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
-        setTimeout(typeEffect, 1200);
+        setTimeout(typeEffect, 80);
     }
 }
 
